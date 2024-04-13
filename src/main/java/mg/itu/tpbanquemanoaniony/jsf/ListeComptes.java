b5dda4c58324090ac661c14c1423081418993a99
Manoaniony <ramarolahymanoaniony@gmail.com>
2024-04-13 22:09:24 +0300

    Page index.xhtml et test de l'application

---
+++ src/main/java/mg/itu/tpbanquemanoaniony/jsf/ListeComptes.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquemanoaniony.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquemanoaniony.entity.CompteBancaire;
+import mg.itu.tpbanquemanoaniony.service.GestionnaireCompte;
+
+/**
+ *
+ * @author ramarolahymanoaniony
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+    private List<CompteBancaire> listeCompte;
+    
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+    public List<CompteBancaire> getAllComptes(){
+        if (listeCompte == null) {
+            listeCompte = gestionnaireCompte.getAllComptes();
+        }
+        return listeCompte;
+    }
+    
+}
