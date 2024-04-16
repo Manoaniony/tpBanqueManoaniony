aae3667e0f3eb37685979e4fbcf46ed410a074ae
Manoaniony <ramarolahymanoaniony@gmail.com>
2024-04-16 14:33:05 +0300

    Nouvelles fonctionnalités : Déposer ou retirer de l'argent sur un compte

--- src/main/java/mg/itu/tpbanquemanoaniony/service/GestionnaireCompte.java
+++ src/main/java/mg/itu/tpbanquemanoaniony/service/GestionnaireCompte.java
@@ -85,17 +85,54 @@ public class GestionnaireCompte {
         return em.find(CompteBancaire.class , id);
     }
 
+    
+    /**
+     * 
+     * @param source
+     * @param destination
+     * @param montant 
+     */
     @Transactional
     public void transferer(CompteBancaire source, CompteBancaire destination, int montant) {
-        source.retirer(montant);
+        /*source.retirer(montant);
         destination.deposer(montant);
         update(source);
         update(destination);
+        */
+        retirer(source,montant);
+        deposer(destination,montant);
     }
 
+    /**
+     * 
+     * @param compteBancaire
+     * @return 
+     */
     @Transactional
     public CompteBancaire update(CompteBancaire compteBancaire) {
         return em.merge(compteBancaire);
+    }
+    
+    /**
+     * Dépôt d'argent sur un compte bancaire.
+     * @param compteBancaire
+     * @param montant 
+     */
+    @Transactional
+    public void deposer(CompteBancaire compteBancaire, int montant) {
+      compteBancaire.deposer(montant);
+      update(compteBancaire);
+    }
+    
+    /**
+     * Retrait d'argent sur un compte bancaire.
+     * @param compteBancaire
+     * @param montant 
+     */
+    @Transactional
+    public void retirer(CompteBancaire compteBancaire, int montant) {
+      compteBancaire.retirer(montant);
+      update(compteBancaire);
     }
 
 }
